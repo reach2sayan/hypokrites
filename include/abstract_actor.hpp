38fde9bbcccc7ff7e030ddc5d5442b771395201d
Sayan <reach2sayan@gmail.com>
2025-07-10 19:19:30 -0400

    first commit

---
+++ include/abstract_actor.hpp
@@ -0,0 +1,8 @@
+//
+// Created by sayan on 7/10/25.
+//
+
+#ifndef ABSTRACT_ACTOR_HPP
+#define ABSTRACT_ACTOR_HPP
+
+#endif //ABSTRACT_ACTOR_HPP
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by sayan on 7/10/25.
+//
+int main() {
+    // This is a placeholder for the main function.
+    // You can add your code here to implement the desired functionality.
+    return 0;
+}
